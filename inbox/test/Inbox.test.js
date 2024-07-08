a3d570b4280dd8c7ec63d951cd8201c5e5fd20a1
Bappi Saha <bappi35-2097@diu.edu.bd>
2024-07-08 22:43:53 +0600

    contract deploy added

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,6 +1,28 @@
 const assert = require('assert');
 const ganache = require('ganache');
 const { Web3 } = require('web3');
+const { interface, bytecode } = require('../compile.js');
 
+const web3 = new Web3(ganache.provider());
 
-// contract test code will go here
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+    // get a list of all account
+
+    accounts = await web3.eth.getAccounts();
+
+    // use one of those accounts to deploy
+    // the contract
+
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there!'] })
+        .send({ from: accounts[0], gas: '1000000' })
+});
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        assert.ok(inbox.options.address)
+    })
+})
