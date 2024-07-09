08d8d3bbcb29ac75b98cb93a1f46913073f52118
Bappi Saha <bappi35-2097@diu.edu.bd>
2024-07-09 07:41:20 +0600

    test added and deploy added

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -1,5 +1,26 @@
+require('dotenv').config();
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const { Web3 } = require('web3');
-//updated web3 and hdwallet-provider imports added for convenience
+const { interface, bytecode } = require('./compile');
 
-// deploy code will go here
+const provider = new HDWalletProvider(process.env.MNEMONIC_KEY, process.env.NETWORK_URL);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+
+    const accounts = await web3.eth.getAccounts();
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hello world!'] })
+        .send({ from: accounts[0], gas: '1000000' })
+    console.log('Contract deployed to', result.options.address);
+
+    const message = await result.methods.message().call();
+    console.log("Intial message", message);
+
+    provider.engine.stop();
+};
+
+deploy();
