31c74abcca38e56175a837bf775bafc1293419a6
Bappi Saha <bappi35-2097@diu.edu.bd>
2024-07-15 17:22:41 +0600

    Lottery project partially complete

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -1,15 +1,79 @@
-import "./App.css";
-import React from "react";
-import web3 from "./web3";
+import "./App.css"
+import React, { useEffect, useState } from "react"
+import web3 from "./web3"
+import lottery from "./lottery"
 
 const App = () => {
-  web3.eth.getAccounts().then(console.log)
-  return (
-    <div className="App">
-      <header className="App-header">
+  const [manager, setManager] = useState('')
+  const [message, setMessage] = useState('')
+  const [players, setPlayers] = useState([])
+  const [balance, setBalance] = useState(0)
+  const [amount, setAmount] = useState(0)
 
-      </header>
-    </div>
-  );
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setMessage("Waiting on transaction success...");
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await lottery.methods.enter().send({
+        from: accounts[0],
+        value: web3.utils.toWei(amount.toString(), 'ether')
+      })
+      setMessage("You have been entered!");
+    } catch (error) {
+      setMessage(error.message);
+    }
+  }
+
+  const pickWinner = async () => {
+    setMessage("Waiting on transaction success...");
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await lottery.methods.pickWinner().send({
+        from: accounts[0]
+      })
+      setMessage("Winner has been picked!");
+    } catch (error) {
+      setMessage(error.message);
+    }
+  }
+
+  const fetchData = async () => {
+    const resManager = await lottery.methods.manager().call();
+    const resPlayers = await lottery.methods.getPlayers().call();
+    const resBalance = await web3.eth.getBalance(lottery.options.address);
+
+    setManager(resManager)
+    setPlayers(resPlayers)
+    setBalance(resBalance)
+  }
+  
+  console.log(manager, players, balance)
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  return (
+   <div>
+    <h2>Lottery Contract</h2>
+    <p>This contract is managed by: {manager}</p>
+    <p>There are currently {players.length} people entered, competing to win {web3.utils.fromWei(balance, 'ether')} ether!</p>
+    <hr/>
+    <form onSubmit={handleSubmit}>
+      <h4>Want to try your luck?</h4>
+      <div>
+        <label>Amount of ether to enter</label>
+        <input type="number" value={amount} onChange={event => setAmount(event.target.value)} />
+      </div>
+      <button type="submit">Enter</button>
+    </form>
+    <hr />
+    <h4>Ready to pick a winner?</h4>
+    <button onClick={() => pickWinner()}>Pick a winner</button>
+    <hr />
+    <h1>{message}</h1>
+   </div>
+  )
 }
-export default App;
+export default App
---
+++ lottery-react/src/lottery.js
@@ -0,0 +1,61 @@
+import web3 from "./web3";
+
+const address = "0x6c3CA85891C096BA7de52d276474B18EEDa98275";
+
+const abi = [
+  {
+    constant: true,
+    inputs: [],
+    name: "manager",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "pickWinner",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getPlayers",
+    outputs: [{ name: "", type: "address[]" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "enter",
+    outputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [{ name: "", type: "uint256" }],
+    name: "players",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+]
+
+const contract = new web3.eth.Contract(abi, address);
+
+export default contract;
