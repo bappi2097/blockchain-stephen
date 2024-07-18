3aa6b9bdd7b59684f440caabb61826d569417275
Bappi Saha <bappi35-2097@diu.edu.bd>
2024-07-18 17:28:35 +0600

    web3 and factory hook added

--- kickstart/ethereum/factory.js
+++ kickstart/ethereum/factory.js
@@ -1,9 +1,22 @@
-import web3 from "./web3";
+import useWeb3 from "./web3";
+import { useEffect, useState } from "react";
 import CampaignFactory from "./build/CampaignFactory.json";
 
-const instance = new web3.eth.Contract(
-    JSON.parse(CampaignFactory.interface),
-    process.env.ADDRESS
-)
 
-export default instance;
+const useFactory = () => {
+    const web3 = useWeb3()
+    const [instance, setInstance] = useState()
+    
+    useEffect(() => {
+        if(web3){
+            setInstance(new web3.eth.Contract(
+                JSON.parse(CampaignFactory.interface),
+                process.env.NEXT_PUBLIC_ADDRESS
+            ))
+        }
+    }, [web3])
+
+    return instance;
+}
+
+export default useFactory;
--- kickstart/ethereum/web3.js
+++ kickstart/ethereum/web3.js
@@ -1,7 +1,13 @@
+import { useEffect, useState } from "react";
 import Web3 from "web3";
 
-window.ethereum.request({ method: "eth_requestAccounts" });
+const useWeb3 = () => {
+    const [web3, setWeb3] = useState()
+    useEffect(() => {
+        window.ethereum.request({ method: "eth_requestAccounts" });
+        setWeb3(new Web3(window.ethereum));
+    }, [])
+    return web3;
+}
 
-const web3 = new Web3(window.ethereum);
-
-export default web3;
+export default useWeb3;
